0dc9983ef1a868bdfb203678f4e052281c8ff443
Rabin Dhamala <rabinkmc@gmail.com>
2023-01-30 15:08:06 +0545

    api test

---
+++ config/urls.py
@@ -0,0 +1,34 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["POST"])
+def test(request):
+    data = {
+        "name": "username",
+        "email": "random@email.com",
+        "password": "rand1234",
+        "link": "http://realhrsoft.com",
+    }
+    return Response(data)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/test/', test)
+]
